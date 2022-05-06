681fdcdca8513595834e608a10cd0e3bc14cac0c
vickylo0919 <jingyilo0919@gmail.com>
2022-05-06 12:01:34 +0800

    add week11

---
+++ week11/week11-3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std ;
+class student
+{
+public:
+	char name[30];
+	int grade;
+};
+int main()
+{
+	int N;
+	cin >> N;
+	vector<student> stu(N);
+	for(int i=0 ; i<N ; i++)
+	{
+		cin >> stu[i].name >> stu[i].grade;
+	}
+	for(int i=0 ; i<N ; i++)
+	{
+		cout << stu[i].name <<" "<< stu[i].grade <<endl;
+	}
+}
