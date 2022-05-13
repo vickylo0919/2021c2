b4f3948c9b3293797196d5fc803cd778736a1e39
vickylo0919 <jingyilo0919@gmail.com>
2022-05-13 14:10:49 +0800

    add week12

---
+++ week12/week12-2.cpp
@@ -0,0 +1,10 @@
+size(300,300);
+background(#DFEA45);
+
+textSize(40);
+
+fill(#456ABC);
+text("Hello",50,50);
+
+fill(#003339);
+text("Hello",50,100);
---
+++ week12/week12-3.cpp
@@ -0,0 +1,11 @@
+void setup()
+{
+  size(300,200);
+  textSize(40);
+}
+void draw()
+{
+  background(#DFEA45);
+  fill(#456ABC);
+  text("Hello",mouseX,mouseY);
+}
